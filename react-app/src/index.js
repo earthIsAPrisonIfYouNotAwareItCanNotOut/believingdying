0f36a53acfe4a6a5153302697b99ed2af5282af0
sb <sb@sbdeMacBook-Pro.local>
2018-04-27 20:46:45 +0800

    persisting the state to the Local Storage

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -10,17 +10,24 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
 
 import reducers from './reducers/reducers'
+import { loadState, saveState } from './localStorage'
 
 import IndexApp from './modules/IndexApp';
 import LoginApp from './modules/LoginApp'
 import MainApp from './modules/MainApp'
 
+const persistedState = loadState()
 const store = createStore(
   reducers,
+  persistedState,
   composeWithDevTools(
     applyMiddleware(thunk)
   )
 )
+
+store.subscribe(() => {
+  saveState(store.getState())
+})
 
 ReactDOM.render(
   <Provider store={store}>
